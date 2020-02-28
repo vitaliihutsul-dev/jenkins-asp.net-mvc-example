53dbbb9b564e00393f0ad367ae038ed404630436
vitalyi.hutsul <vitalyi.hutsul@unity-bars.com>
2020-02-28 14:40:30 +0200

    fix err

--- UnionMVC/UnionMVC/Startup.cs
+++ UnionMVC/UnionMVC/Startup.cs
@@ -33,7 +33,7 @@ namespace UnionMVC
 
             ///////eeee
 
-            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
