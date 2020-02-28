34242cbeb79d40e9ea77ceef37960e6b3d0c9b84
vitalyi.hutsul <vitalyi.hutsul@unity-bars.com>
2020-02-28 14:43:20 +0200

    test

--- UnionMVC/UnionMVC/Startup.cs
+++ UnionMVC/UnionMVC/Startup.cs
@@ -31,7 +31,7 @@ namespace UnionMVC
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            ///////eeee
+            ///////eeeewwww
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
